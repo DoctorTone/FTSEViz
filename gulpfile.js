ec76b1203a44f3f5c612973ff261490ca25e9855
DoctorTone <drtonesoftware@gmail.com>
2017-10-19 15:18:39 +0100

    Minified three js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
 
-gulp.task("build", function() {
-    return gulp.src("./js/FTSE.js")
+gulp.task("build", ["compile", "min-copy"], function() {
+
+});
+
+gulp.task("compile", function() {
+    return gulp.src(["./js/*.js", "!./js/*.min.js"])
         .pipe(babel())
         .pipe(gulp.dest("./dist/js"));
+});
+
+gulp.task("min-copy", function() {
+    return gulp.src("./js/*.min.js")
+        .pipe(gulp.dest("./dist/js"))
 });
