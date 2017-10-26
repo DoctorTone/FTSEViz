2cf283e0293e70ce81ff99b974497a84fed2fb9d
DoctorTone <drtonesoftware@gmail.com>
2017-10-26 06:02:16 +0100

    Added minifier packages.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,43 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
+var changed = require("gulp-changed");
+var uglify = require("gulp-uglify");
+var pump = require("pump");
+var cleanCSS = require("gulp-clean-css");
+var htmlmin = require("gulp-htmlmin");
 
 gulp.task("build", ["compile", "min-copy"], function() {
 
 });
 
+var DEST = "./temp/js";
 gulp.task("compile", function() {
     return gulp.src(["./js/*.js", "!./js/*.min.js"])
+        .pipe(changed(DEST))
         .pipe(babel())
-        .pipe(gulp.dest("./dist/js"));
+        .pipe(gulp.dest(DEST));
+});
+
+gulp.task("compress", function( callback ) {
+    pump([
+        gulp.src("temp/js/*.js"),
+        uglify(),
+        gulp.dest("./build/js")
+    ],
+        callback
+    );
+});
+
+gulp.task("compress-css", function() {
+    gulp.src("temp/css/*.css")
+        .pipe(cleanCSS())
+        .pipe(gulp.dest("build/css"))
+});
+
+gulp.task("compress-html", function() {
+    gulp.src("temp/*.html")
+        .pipe(htmlmin( {collapseWhitespace: true} ))
+        .pipe(gulp.dest("build"))
 });
 
 gulp.task("min-copy", function() {
@@ -18,7 +47,7 @@ gulp.task("min-copy", function() {
 
 gulp.task("copy", function() {
     gulp.src("*.html")
-        .pipe(gulp.dest("./dist"));
+        .pipe(gulp.dest("./temp"));
     gulp.src("css/*.css")
-        .pipe(gulp.dest("dist/css/"))
+        .pipe(gulp.dest("temp/css/"))
 });
